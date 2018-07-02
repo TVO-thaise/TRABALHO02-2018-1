7f1222d27d444c2351630c875e8489533232294a
TVO-thaise <thaisevilaca2013@gmail.com>
2018-07-02 15:17:51 -0300

    incluindo dependencia - CardView

--- app/src/main/java/com/example/samsung/appcidadao/activity/MainActivity.java
+++ app/src/main/java/com/example/samsung/appcidadao/activity/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.samsung.appcidadao.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -15,45 +18,49 @@ import com.example.samsung.appcidadao.R;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
 
+    private String[] activities = {"ListaEleitor"};
+    private String[] itemMenu = {"Eleitor"};
+    private String[] activities = {"ListaCandidato"};
+    private String[] itemMenu = {"Candidato"};
+
 
 
     private RadioGroup rgCidadao;
     private RadioButton rbEleitor;
-    private RadioButton rbCandidato;
     private ImageView imgCidadao;
     private Integer []imagens = { R.drawable.cidadao};
     private Button btAdd;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.imgCidadao = (ImageView) findViewById(R.id.imgCidadao);
-        this.rgCidadao = (RadioGroup) findViewById(R.id.rgCidadao);
-        this.rgCidadao.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                RadioButton radioButton = (RadioButton) radioGroup.findViewById(i);
-                int index = radioGroup.indexOfChild(radioButton);
-                imgCidadao.setImageResource(imagens[index]);
-            }
-        });
 
-        final RadioButton rbEleitor = (RadioButton) findViewById(R.id.rbEleitor);
-        final RadioButton rbCandidato = (RadioButton) findViewById(R.id.rbCandidato);
-        this.btAdd = (Button) findViewById(R.id.btAdd);
-        btAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemMenu);
 
-            }
-                if (rbEleitor.isChecked() == true) {
-                    Toast.makeText(getBaseContext(), "O Cidadao é um Eleitor!", Toast.LENGTH_SHORT).show();
-                } else if (rbCandidato.isChecked() == true) {
-                    Toast.makeText(getBaseContext(), "O Cidadao é um Candidato!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+            ListView listView = (ListView) findViewById(R.id.ListaEleitor);
+            ListView listView = (ListView) findViewById(R.id.ListaCandidato);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(this);
+        }
 
-    }
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+            Intent intent = null;
+
+            try {
+
+                Class obj = Class.forName ("com.example.samsung.appcidadao.activity."+activities[position]);
+                intent = new Intent(this, obj);
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            startActivity(intent);
+
+        }
+
+}
